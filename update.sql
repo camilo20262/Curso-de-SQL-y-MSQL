706586f054a86ae04a4844ca1820f528e9eff918
Camilo Diaz <camilo20263@gmail.com>
2025-11-19 12:59:11 -0500

    alter table

---
+++ update.sql
@@ -0,0 +1,6 @@
+update clients set gender  ='f' where rand () >.51;
+update clients set gender = 'm'  where gender = 'ns';
+
+update clients set country  ='col' where rand () <.31;
+update clients set country  ='us' where rand () <.15;
+update clients set country  ='ar' where rand () <.15 and country in ('mx','us');
