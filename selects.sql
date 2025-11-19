b2edb66cbf61955552c86e2769e6f0585d947ef1
Camilo Diaz <camilo20263@gmail.com>
2025-11-19 13:34:55 -0500

    contar paises y genero

--- selects.sql
+++ selects.sql
@@ -4,3 +4,8 @@ select country, count(*) as total
 from clients
 group by country
 order by total;
+-- Count clients by country and gender
+select country, gender, count(*) as total
+from clients
+group by country,gender
+order by total;
