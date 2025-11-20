026feb7e9929f8f217696ba503e10d542b1f2a9f
Camilo Diaz <camilo20263@gmail.com>
2025-11-20 14:44:31 -0500

    cambio de prueba

--- selects.sql
+++ selects.sql
@@ -9,3 +9,14 @@ select country, gender, count(*) as total
 from clients
 group by country,gender
 order by total;
+--HOLS
+
+-- Select all from example
+select length(description) as longitud_description
+from products;
+
+--Reemplazar espacios por guiones: Podemos usar expresiones regulares con REGEXP_REPLACE()
+SELECT name, REGEXP_REPLACE(LOWER(name), ' ', '-') AS generated, slug
+FROM products 
+LIMIT 10;
+
