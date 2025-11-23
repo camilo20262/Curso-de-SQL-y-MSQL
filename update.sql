f02d178cb0ec75ba701e45ec3fe9a6bdc477b33c
Camilo Diaz <camilo20263@gmail.com>
2025-11-23 12:24:39 -0500

    update con select incluido

--- update.sql
+++ update.sql
@@ -4,3 +4,13 @@ update clients set gender = 'm'  where gender = 'ns';
 update clients set country  ='col' where rand () <.31;
 update clients set country  ='us' where rand () <.15;
 update clients set country  ='ar' where rand () <.15 and country in ('mx','us');
+
+
+---
+UPDATE clients AS c
+SET bill_count = (
+    SELECT COUNT(*)
+    FROM bills AS b
+    WHERE b.client_id = c.client_id
+);
+
