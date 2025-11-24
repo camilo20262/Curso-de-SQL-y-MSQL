be9230b7cbf2be82b1e9581d0ed7308ad3f61f66
Camilo Diaz <camilo20263@gmail.com>
2025-11-24 13:19:57 -0500

    agregar json

--- objetos_json.sql
+++ objetos_json.sql
@@ -1,3 +1,9 @@
---s
+-- Agregar una columna JSON a la tabla products
 ALTER TABLE products 
 ADD COLUMN data JSON AFTER price;    
+
+
+-- Actualizar la columna JSON con datos específicos para un producto
+UPDATE products 
+SET data = '{"brand": "Pear", "HDSize": "40 gigabytes"}' 
+WHERE product_id = 10;
