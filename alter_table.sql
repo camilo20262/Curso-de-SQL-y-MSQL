a6a209a61672321534682e730e81c60704e2937d
Camilo Diaz <camilo20263@gmail.com>
2025-11-24 13:14:14 -0500

    json

--- alter_table.sql
+++ alter_table.sql
@@ -1,2 +1,5 @@
 Alter table clients add column gender ENUM('m','f','ns') default 'ns' not null;
 alter table clients add column country varchar(2) default 'mx' not null;    
+
+
+alter table products add column description_length integer as (length(description));
--- selects.sql
+++ selects.sql
@@ -9,3 +9,13 @@ select country, gender, count(*) as total
 from clients
 group by country,gender
 order by total;
+
+
+-- Select all from example
+select length(description) as longitud_description
+from products;
+
+--Reemplazar espacios por guiones: Podemos usar expresiones regulares con REGEXP_REPLACE()
+SELECT name, REGEXP_REPLACE(LOWER(name), ' ', '-') AS generated, slug
+FROM products 
+LIMIT 10;
